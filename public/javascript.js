c5ad1ba688d5c18d2b00ef63b235a02f37f4c5bf
Anuj Kothari <anujkothari@Anujs-MacBook-Pro.local>
2018-11-27 23:23:04 -0500

    added ability to add songs to playlist

--- public/javascript.js
+++ public/javascript.js
@@ -1,3 +1,5 @@
+var trackURIs = "";
+
 function setMood() {
   var mood = $(".same-as-selected").text();
 
@@ -60,9 +62,8 @@ function retrieveRecommendations() {
     });
   }
 
+
   var accessToken = $("#accessToken").val();
-
-
   $.ajax({
     url: 'https://api.spotify.com/v1/recommendations?' + param,
     type: 'GET',
@@ -71,6 +72,36 @@ function retrieveRecommendations() {
     },
     success: function(data) {
       $("#recommendationDiv").empty();
+      var outerDiv = $('<div/>', {
+        class: "col-xs-12"
+      });
+      var playlistInputDiv = $('<div/>', {
+        class: "col-xs-6"
+      });
+      var playlistInput = $('<input/>', {
+        id: "playlistName",
+        attr: {
+          "type": "text",
+          "placeholder":"Enter Playlist Name",
+          "style":"border:0; border-radius:4px; padding:8px;"
+        }
+      });
+      playlistInputDiv.append(playlistInput);
+      var createButtonDiv = $('<div/>', {
+        class: "col-xs-6"
+      });
+      var createButton = $('<button/>', {
+        class:"btn btn-lg",
+        attr: {
+          "type": "button",
+          "onclick":"createPlaylist();"
+        },
+        text:"Add Songs to Playlist"
+      });
+      createButtonDiv.append(createButton);
+      outerDiv.append(playlistInputDiv);
+      outerDiv.append(createButtonDiv);
+      $("#recommendationDiv").append(outerDiv);
       $("#recommendationDiv").css("display", "block");
       $("#informationDiv").css("display", "none");
       console.log(data);
@@ -80,6 +111,7 @@ function retrieveRecommendations() {
         albumName = data.tracks[i].album.name;
         imageURL = data.tracks[i].album.images[2].url;
         openURL = data.tracks[i].external_urls.spotify;
+        trackURIs += data.tracks[i].uri + ",";
         addTrackUI(trackName, artistName, albumName, imageURL, openURL);
       }
     }
@@ -119,6 +151,7 @@ function addTrackUI(trackName, artistName, albumName, albumImg, openURL) {
     },
     text: trackName
   });
+  anchor.append(songName);
   var trackArtist = $('<div/>', {
     class: "trackDetail artistAlbum",
     attr: {
@@ -133,15 +166,15 @@ function addTrackUI(trackName, artistName, albumName, albumImg, openURL) {
     },
     text: albumName
   });
-  trackInfo.append(songName);
+  trackInfo.append(anchor);
   trackInfo.append(trackArtist);
   trackInfo.append(trackAlbum);
   trackDiv.append(imgDiv);
   trackDiv.append(trackInfo);
   outerDiv.append(trackDiv);
-  anchor.append(outerDiv);
 
-  $("#recommendationDiv").append(anchor);
+
+  $("#recommendationDiv").append(outerDiv);
 
 
 }
@@ -151,6 +184,84 @@ function displayInstruction() {
   $("#informationDiv").css("display", "block");
 }
 
-function showError() {
+function getUserID() {
+  var accessToken = $("#accessToken").val();
+  $.ajax({
+    url: 'https://api.spotify.com/v1/me',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken,
+      'Content-Type': 'application/json'
+    },
+    success: function(response) {
+      console.log(response.id);
+    },
+    error: function() {
+      alert('there was an error');
+    }
+  });
+}
+
+function createPlaylist() {
+
+  var accessToken = $("#accessToken").val();
+  var userID = $("#userID").val();
+  //var playllistName = $("#genre").val() + $("#mood").val();
+  var playlistName = $("#playlistName").val();
+  if(playlistName){
+    var data = {
+      name: playlistName
+    };
+    var playlistID = "";
+    $.ajax({
+      type: 'POST',
+      url: 'https://api.spotify.com/v1/users/' + userID + '/playlists',
+      data: JSON.stringify(data),
+      dataType: 'json',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      contentType: 'application/json',
+      success: function(result) {
+        console.log(result);
+        playlistID = result.id;
+        addToPlaylist(playlistID);
+      },
+      error: function(e) {
+        console.log(e);
+      }
+    });
+  }
+  else{
+    swal({
+      type: 'error',
+      title: 'Oops...',
+      text: 'Please enter a name for the playlist'
+    });
+  }
+}
+
+function addToPlaylist(playlistID){
+  var accessToken = $("#accessToken").val();
+  console.log(trackURIs);
+  var u = 'https://api.spotify.com/v1/playlists/'+ playlistID + '/tracks?uris=' + trackURIs;
+  console.log(u);
+
+  $.ajax({
+    type: 'POST',
+    url: 'https://api.spotify.com/v1/playlists/'+ playlistID + '/tracks?uris=' + trackURIs,
+    dataType: 'text',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    },
+    contentType: 'application/json',
+    success: function(result) {
+      console.log(result);
+      $("#playlistName").val("");
+    },
+    error: function(e) {
+      console.log(e);
+    }
+  });
+
 
 }
